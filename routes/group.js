360a34e06bc7032f00ea0aee67c947b46f1130f3
kailasbk <31834888+kailasbk@users.noreply.github.com>
2020-05-18 16:33:42 -0400

    Revert "Add changes for initial test deploy"

--- routes/group.js
+++ routes/group.js
@@ -1,39 +1,20 @@
 const express = require('express');
 const client = require('../db');
 const auth = require('../middleware/auth');
-const transport = require('../email');
 
-// mounted at /group
 const group = express.Router();
-group.use(auth);
 
-group.get('/all', async (req, res) => {
+group.use('/all', auth, async (req, res) => {
 	try {
 		const data = await client.query(`
-			SELECT owner, members
-			FROM Groups
-			WHERE owner=$1 OR members@>ARRAY[$1];`,
-			[req.token.client_id]
-		);
-
-		let members = [];
-		data.rows.forEach(entry => {
-			members = members.concat(([entry.owner].concat(entry.members)));
-		});
-
-		let sorted = [];
-		members.forEach(value => {
-			if (!sorted.includes(value) && value !== req.token.client_id) {
-				sorted.push(value);
-			}
-		})
-		members = sorted;
+			SELECT username, firstname, lastname, email, picture, audio
+			FROM Users;
+		`);
+		const members = data.rows;
 
 		res.json({
-			name: 'All groups',
-			owner: req.token.client_id,
-			members: members,
-			me: req.token.client_id
+			name: "John Doe's Group",
+			members: members
 		});
 	}
 	catch (err) {
@@ -42,44 +23,17 @@ group.get('/all', async (req, res) => {
 	}
 });
 
-group.post('/new', express.json(), async (req, res) => {
-	try {
-		const members = req.body.members ? req.body.members : [];
-		await client.query(`
-			INSERT INTO Groups (name, owner, members)
-			VALUES ($1, $2, $3);`,
-			[req.body.name, req.token.client_id, members]
-		);
-
-		res.sendStatus(204);
-	}
-	catch (err) {
-		console.log(err);
-		if (err.constraint === 'groups_owner_fkey') {
-			res.sendStatus(400);
-		}
-		res.sendStatus(500);
-	}
-});
-
-group.get('/:id', async (req, res) => {
+group.use('/new', auth, async (req, res) => {
 	try {
 		const data = await client.query(`
-			SELECT name, owner, members
-			FROM Groups
-			WHERE id=$1;`,
-			[req.params.id]
-		);
-		const name = data.rows[0].name;
-		const owner = data.rows[0].owner;
-		const members = data.rows[0].members;
-		const sorted = members.sort();
+			SELECT username, firstname, lastname, email, picture, audio
+			FROM Users;
+		`);
+		const members = data.rows;
 
 		res.json({
-			name: name,
-			owner: owner,
-			members: sorted,
-			me: req.token.client_id
+			name: "John Doe's Group",
+			members: members
 		});
 	}
 	catch (err) {
@@ -88,81 +42,18 @@ group.get('/:id', async (req, res) => {
 	}
 });
 
-group.post('/:id/invite', express.json(), async (req, res) => {
+group.use('/:id', auth, async (req, res) => {
 	try {
-		const data = await client.query('SELECT owner FROM Groups WHERE id=$1;', [req.params.id]);
-		const owner = data.rows[0].owner;
+		const data = await client.query(`
+			SELECT username, firstname, lastname, email, picture, audio
+			FROM Users;
+		`);
+		const members = data.rows;
 
-		if (owner === req.token.client_id) {
-			const emails = req.body.emails;
-			let valueString = '';
-			emails.forEach((value, index) => {
-				valueString += `('${req.params.id}', '${value}')`;
-				if (index < emails.length - 1) {
-					valueString += ',';
-				}
-			});
-
-			await client.query(`
-				INSERT INTO Invites (groupId, email)
-				VALUES ${valueString};`,
-			);
-
-			let emailData = emails.map(value =>
-				({
-					to: value,
-					content: `
-							<p> Hey there! <p>
-							<p> You have been invited to join a pronouncit group! </p>
-							<p> <u> Already a user? </u> </p>
-							<p> Login here: <a href="www.pronouncit.app/login"> www.pronouncit.app/login </a> </p>
-							<p> <u> Don't have an account? </u> </p>
-							<p> Signup here: <a href="www.pronouncit.app/register?email=${value}"> www.pronouncit.app/register <a> </p>
-							`
-				})
-			);
-
-			const promises = emailData.map(value => {
-				return transport.sendMail({
-					from: "bot@pronouncit.app",
-					to: value.to,
-					subject: "New pronouncit invite",
-					html: value.content
-				});
-			})
-
-			await Promise.all(promises);
-
-			res.sendStatus(204);
-		}
-		else {
-			res.sendStatus(401);
-		}
-	}
-	catch (err) {
-		console.log(err);
-		res.sendStatus(500);
-	}
-});
-
-group.post('/:id/remove', express.json(), async (req, res) => {
-	try {
-		req.body.members.forEach(async (member) => {
-			await client.query(`
-		 		UPDATE Groups
-				SET members=array_remove(members, $1)
-				WHERE id=$2 AND owner=$3;`,
-				[member, req.params.id, req.token.client_id]
-			);
-
-			await client.query(`
-				DELETE FROM Invites
-				WHERE email IN (SELECT email FROM Users WHERE username=$1)`,
-				[member]
-			);
+		res.json({
+			name: "John Doe's Group",
+			members: members
 		});
-
-		res.sendStatus(204);
 	}
 	catch (err) {
 		console.log(err);
@@ -170,28 +61,37 @@ group.post('/:id/remove', express.json(), async (req, res) => {
 	}
 });
 
-group.delete('/:id', async (req, res) => {
+group.use('/:id/add', auth, async (req, res) => {
 	try {
-		const data = await client.query('SELECT owner FROM Groups WHERE id=$1;', [req.params.id]);
-		const owner = data.rows[0].owner;
+		const data = await client.query(`
+			SELECT username, firstname, lastname, email, picture, audio
+			FROM Users;
+		`);
+		const members = data.rows;
 
-		if (owner === req.params.id) {
-			await client.query(`
-				DELETE FROM Groups
-				WHERE id=$1 AND owner=$2`,
-				[req.params.id, req.token.client_id]
-			);
+		res.json({
+			name: "John Doe's Group",
+			members: members
+		});
+	}
+	catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
+});
 
-			await client.query(`
-				DELETE FROM Invites
-				WHERE groupid=$1;`,
-				[req.params.id]
-			);
-			res.sendStatus(204);
-		}
-		else {
-			res.sendStatus(401);
-		}
+group.use('/:id/delete', auth, async (req, res) => {
+	try {
+		const data = await client.query(`
+			SELECT username, firstname, lastname, email, picture, audio
+			FROM Users;
+		`);
+		const members = data.rows;
+
+		res.json({
+			name: "John Doe's Group",
+			members: members
+		});
 	}
 	catch (err) {
 		console.log(err);
--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,22 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const client = require('../db');
 const auth = require('../middleware/auth');
-const users = require('../db/tables/users');
 
-// mounted at /user
 const user = express.Router();
-user.use(auth);
 
-user.get('/:id', async (req, res) => {
+user.post('/register', express.json(), async (req, res) => {
 	try {
-		const info = await users.getInfo(req.params.id);
-		res.json(info);
+		await client.query(`
+			INSERT INTO Users (username, firstname, lastname, email, password)
+			VALUES ($1, $2, $3, $4, $5);`,
+			[req.body.username, req.body.firstname, req.body.lastname, req.body.email, req.body.password]
+		);
+		res.sendStatus(204);
 	} catch (err) {
-		console.log(err);
-		if (err === 'Not Found') {
-			res.sendStatus(404);
+		console.error(err);
+		if (err.constraint === 'users_pkey') {
+			res.sendStatus(400);
 		}
 		else {
 			res.sendStatus(500);
@@ -22,35 +24,67 @@ user.get('/:id', async (req, res) => {
 	}
 });
 
-user.get('/:id/picture', async (req, res) => {
+user.post('/login', express.json(), async (req, res) => {
 	try {
-		const picture = await users.getPicture(req.params.id);
-		res.type('image/jpeg');
-		res.send(picture);
-	} catch (err) {
-		console.log(err);
-		if (err === 'Not Found') {
-			res.sendStatus(404);
+		const data = await client.query(`
+			SELECT username, email
+			FROM Users
+			WHERE username=$1 AND password=$2;`, // include hash later on
+			[req.body.username, req.body.password]
+		);
+		if (data.rows.length === 1) {
+			const claims = data.rows[0];
+			const token = jwt.sign({ client_id: claims.username, email: claims.email }, 'secret');
+			res.send(token);
 		}
 		else {
-			res.sendStatus(500);
+			res.sendStatus(400);
 		}
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
 	}
 });
 
-user.get('/:id/audio', async (req, res) => {
+user.get('/me', auth, async (req, res) => {
 	try {
-		const audio = await users.getAudio(req.params.id);
-		res.type('audio/ogg');
-		res.send(audio);
-	} catch (err) {
-		console.log(err);
-		if (err === 'Not Found') {
-			res.sendStatus(404);
+		const data = await client.query(`
+			SELECT username, firstname, lastname, email, picture, audio 
+			FROM Users
+			WHERE UserName=$1;`,
+			[req.token.client_id]
+		);
+		if (data.rows.length === 1) {
+			var user = data.rows[0];
+			res.json(user);
 		}
 		else {
-			res.sendStatus(500);
+			res.sendStatus(404);
 		}
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
+});
+
+user.get('/:id', auth, async (req, res) => {
+	try {
+		const data = await client.query(`
+			SELECT username, firstname, lastname, email, picture, audio 
+			FROM Users
+			WHERE UserName=$1;`,
+			[req.params.id]
+		);
+		if (data.rows.length === 1) {
+			var user = data.rows[0];
+			res.json(user);
+		}
+		else {
+			res.sendStatus(404);
+		}
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
 	}
 });
 
